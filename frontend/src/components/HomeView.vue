be1c8da555bc82401394e4084587b9b9805d7259
Risshab Srinivas <risshabsrinivas@Risshabs-MacBook-Pro.local>
2025-03-14 18:17:22 +0530

    First Vue Screen

---
+++ frontend/src/components/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Welcome to the HouseHold Services app</h1> 
+        <router-link to="/admin" class="btn btn-primary"></router-link>       
+    </div>
+</template>
+
+<script>
+export default{
+    name:'homeView'
+}
+</script>

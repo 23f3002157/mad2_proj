eb695e9e89888ce87dd20d13d64baa25453da626
Risshab Srinivas <risshabsrinivas@Risshabs-MacBook-Pro.local>
2025-03-22 21:34:36 +0530

    Added celery jobs - daily and monthly

--- backend/app_src/task.py
+++ backend/app_src/task.py
@@ -6,12 +6,12 @@ from jinja2 import Template
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+from .models import db, CustomerDetails, Service, ServiceCategory, Servicer, ServiceRequest, Feedback
 
 def send_mail(email,email_content):
     smtp_server_host = "localhost"
     smtp_port = 1025
-    sender_email = "admin@email.com"
+    sender_email = "admin@gmail.com"
     sender_password = ""
 
     # Set up email server
@@ -20,7 +20,7 @@ def send_mail(email,email_content):
     msg = MIMEMultipart()
     msg["From"] = sender_email
     msg["To"] = email
-    msg["Subject"] = "Remainders"
+    msg["Subject"] = "Remainders - Pending Service Requests"
 
     # Attach the HTML content to the email
     msg.attach(MIMEText(email_content, "html"))
@@ -28,26 +28,24 @@ def send_mail(email,email_content):
     print("Mail Sent")
 
 
-def get_html_report(user, data):
-    with open("app_src/report.html", "r") as file:
+def get_html_report(data, name, filename):
+    with open("app_src/"+filename+".html", "r") as file:
         jinja_template = Template(file.read())
-        html_report = jinja_template.render({"user":"helo","data":"data"})
+        html_report = jinja_template.render({"user":name,"data":data})
         return html_report
 
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
-    sender.add_periodic_task(30.0, daily_remainder.s(), name='add every 30')
-
-    # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
-
-    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(30.0, monthly_reminder.s(), name='add every 30')
     sender.add_periodic_task(
-        crontab(hour=18, minute=30),
+        crontab(hour=21, minute=12),
         daily_remainder.s(),
         name="Daily Remainder at 6:30 PM"
+    )
+    sender.add_periodic_task(
+        crontab(hour=21, minute=30),
+        monthly_reminder.s(),
+        name="Monthly Customer Reminder"
     )
 
 @celery.task
@@ -56,11 +54,36 @@ def test(arg):
 
 @celery.task
 def daily_remainder():
-    customer = CustomerDetails.query.all()
-    for cust in customer:
-        print(cust.convert_to_json())
-    x=cust
-    html_report = get_html_report("hello", "there")
-    send_mail("risshabsrinivas@gmail.com",html_report)
-    print(x.name)
+    serProf = Servicer.query.all()
+    for curr in serProf:
+        s1=ServiceRequest.query.filter_by(servicer_id=curr.servicer_ID).all()
+        if s1:
+            pendingRequests = []
+            for serReq in s1:
+                if serReq.status == "REQUESTED":
+                    s2=Service.query.filter_by(service_ID=serReq.service_id).first()
+                    print(s2.service_Description)
+                    d=serReq.convert_to_json()
+                    d["service_Description"] = s2.service_Description
+                    pendingRequests.append(d)
+            
+            report = get_html_report(pendingRequests, curr.firstname+" "+curr.lastname, "report")
+            send_mail(curr.email, report)
 
+
+@celery.task
+def monthly_reminder():
+    c=CustomerDetails.query.all()
+    for cust in c:
+        cust_ids = cust.cust_id
+        s1 = ServiceRequest.query.filter_by(cust_id=cust_ids).all()
+        if s1:
+            service_requests = []
+            for i in s1:
+                s2 = Service.query.filter_by(service_ID=i.service_id).first()
+                x=s2.service_Description
+                d=i.convert_to_json()
+                d['service_Description'] = x
+                service_requests.append(d)
+            report = get_html_report(service_requests, cust.name, "cust_report")
+            send_mail(cust.email, report)
